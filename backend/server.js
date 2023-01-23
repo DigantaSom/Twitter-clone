7ec8be4bff2975b75fbead5f774d0c00519a94a3
Diganta Som <itsdsom111@gmail.com>
2023-01-23 16:47:05 +0530

    Backend - Connecting to MongoDB with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const connectDB = require('./config/dbConn');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -12,10 +18,10 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-// routes
+// Routes
 app.use('/', require('./routes/root.routes'));
 
-// fallback
+// Fallback
 app.all('*', (req, res) => {
   res.status(404);
   if (req.accepts('html')) {
@@ -27,6 +33,13 @@ app.all('*', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+// MongoDB connection
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB...');
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
+});
+mongoose.connection.on('error', err => {
+  console.log('Error connecting to MongoDB:', err);
 });
