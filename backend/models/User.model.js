c7bfaf564c544fe59809f1f25a0a7452f4787c21
Diganta Som <itsdsom111@gmail.com>
2023-01-23 16:53:08 +0530

    Backend - Creating the User and Tweet models

---
+++ backend/models/User.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    handle: { type: String, required: true, unique: true },
+    handle_lowercase: { type: String, required: true, unique: true },
+    profilePicture: { type: String },
+    birthday: { type: Date },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('user', userSchema);
